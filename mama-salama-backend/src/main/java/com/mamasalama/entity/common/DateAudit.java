0e1654c41737c1c537f80d01f0f2170d8c67b779
mariam-01 <mariam.larhlid2001@gmail.com>
2026-07-19 21:41:17 +0100

    Refactor `DateAudit` to optimize imports and annotations in `backend` module

--- mama-salama-backend/src/main/java/com/mamasalama/entity/common/DateAudit.java
+++ mama-salama-backend/src/main/java/com/mamasalama/entity/common/DateAudit.java
@@ -1,6 +1,9 @@
 package com.mamasalama.entity.common;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +13,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
