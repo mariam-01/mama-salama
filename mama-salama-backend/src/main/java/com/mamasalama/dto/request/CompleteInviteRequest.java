10d544bf836e760462b29b6a585ae498d8b47f84
mariam-01 <mariam.larhlid2001@gmail.com>
2026-09-06 01:57:44 +0100

    Add `CompleteInviteRequest` DTO with validation for completing invite flow

---
+++ mama-salama-backend/src/main/java/com/mamasalama/dto/request/CompleteInviteRequest.java
@@ -0,0 +1,22 @@
+package com.mamasalama.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompleteInviteRequest {
+
+    @NotBlank(message = "Invite token is required")
+    private String token;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    private String phone;
+}
