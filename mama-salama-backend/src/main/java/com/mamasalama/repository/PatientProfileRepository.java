dfb78f838c6bee34d1a945d989817a1478a6e2a8
mariam-01 <mariam.larhlid2001@gmail.com>
2026-09-06 02:19:04 +0100

    Add `findByAssignedDoctor` and `countByAssignedDoctor` methods in `PatientProfileRepository`

--- mama-salama-backend/src/main/java/com/mamasalama/repository/PatientProfileRepository.java
+++ mama-salama-backend/src/main/java/com/mamasalama/repository/PatientProfileRepository.java
@@ -4,10 +4,13 @@ import com.mamasalama.entity.PatientProfile;
 import com.mamasalama.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PatientProfileRepository extends JpaRepository<PatientProfile, UUID> {
     Optional<PatientProfile> findByUser(User user);
     boolean existsByUser(User user);
+    List<PatientProfile> findByAssignedDoctor(User doctor);
+    long countByAssignedDoctor(User doctor);
 }
